8f6d6c02105f332991520e524f7bd1ef5b6e2957
cjparsonson <cjparsonson@gmail.com>
2024-08-26 23:03:13 +0100

    Implement DDA line drawing and triangle drawing.

--- 3DRenderer/Display.h
+++ 3DRenderer/Display.h
@@ -19,6 +19,8 @@ bool initialize_window(void);
 void draw_grid(int gridSize);
 void draw_point_grid(int gridSize);
 void draw_pixel(int x, int y, uint32_t colour);
+void draw_line(int x0, int y0, int x1, int y1, uint32_t colour);
+void draw_triangle(int x0, int y0, int x1, int y1, int x2, int y2, uint32_t colour);
 void draw_rect(int x, int y, int width, int height, uint32_t colour);
 void render_colour_buffer(void);
 void clear_colour_buffer(uint32_t colour);
